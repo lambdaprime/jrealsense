f0e78cb8dbd2c3c07c26b19427d16655707b88b0
lambdaprime <intid@protonmail.com>
2021-02-14 19:49:14 -0800

    Updating links in javadoc

--- jrealsense/src/main/java/module-info.java
+++ jrealsense/src/main/java/module-info.java
@@ -2,7 +2,8 @@
  * <p>Java module which allows to interact with
  * Intel RealSense cameras.</p>
  * 
- * @see <a href="https://github.com/lambdaprime/jrealsense">jrealsense on github</a>
+ * @see <a href="https://github.com/lambdaprime/jrealsense/releases">Download</a>
+ * @see <a href="https://github.com/lambdaprime/jrealsense">GitHub repository</a>
  * 
  */
 module jrealsense {
