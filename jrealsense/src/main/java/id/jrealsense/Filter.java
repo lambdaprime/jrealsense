3b2aab900952910d2325c8bf08121d1487089b1c
lambdaprime <intid@protonmail.com>
2021-02-21 19:36:05 -0800

    Adding comments

--- jrealsense/src/main/java/id/jrealsense/Filter.java
+++ jrealsense/src/main/java/id/jrealsense/Filter.java
@@ -1,5 +1,11 @@
 package id.jrealsense;
 
+/**
+ * It is implementation details of the filter how to do the processing.
+ * 
+ * Standard way is to ask processing block to process the frame and poll
+ * the processed frame from internal queue once it is ready.
+ */
 public interface Filter<IN extends Frame<IN>, OUT extends Frame<?>> extends AutoCloseable {
     
     /**
